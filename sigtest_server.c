f25006beb32c41f9ef159becdbf5f367b2d3568d
Taku Kawasaki <takukawasaki@realm00.com>
2016-04-26 01:19:08 +0900

    signal-test-server

---
+++ sigtest_server.c
@@ -0,0 +1,39 @@
+#include "net_simplelib.h"
+
+int *argc_;
+char ***argv_;
+char ***envp_;
+
+void
+sigHangupHandler(int sig)
+{
+     err_print("sigHangupHandler(%d)\n",sig);
+     if ((execve((*argv_)[0],(*argv_),(*envp_))) == -1) {
+          err_sys("execve");
+     }
+}
+
+
+int main(int argc, char *argv[],char *envp[])
+{
+
+     int sock;
+     if (argc <= 1) {
+          err_quit("sigtest_server port\n");
+     }
+
+     argc_ = &argc;
+     argv_ = &argv;
+     envp_ = &envp;
+
+     Signal(SIGHUP, sig_chld);
+     err_print("Signal():end\n");
+     
+     sock = server_socket(argv[1],argv[2]);
+
+     accept_loop(sock);
+
+     close(sock);
+               
+     return 0;
+}
